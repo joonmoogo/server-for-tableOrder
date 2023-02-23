0304b4996e4576e9c1823eae9f44d5c54b2081c7
joonmoogo <junemuk10@gmail.com>
2023-02-24 01:09:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+ const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+  const cors = require('cors');
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Im server')
+})
+
+app.get('/data',(req,res)=>{
+    res.json({name:'oh',age:26,major:'computer science'})
+})
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} has entered`);
+  socket.on('order',(data)=>{
+    console.log(data)
+    io.emit('userOrder',data);
+  })
+});
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
